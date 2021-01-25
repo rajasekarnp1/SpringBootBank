cb843a9a7767ffd852b3a24c0bbf4661fd77b596
rajasekarn <rajasekarn76@gmail.com>
2021-01-25 09:23:25 +0530

    new changes

---
+++ spring-boot-application-run/src/main/java/com/javatpoint/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.scb.bank;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController 
+{
+@RequestMapping("/")
+public String hello() 
+{
+return "Hello User";
+}
+}
